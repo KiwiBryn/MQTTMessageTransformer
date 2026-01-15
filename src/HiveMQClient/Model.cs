237770b09bb0af59759ea5907ca614cac519759e
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2026-01-16 12:17:41 +1300

    Added "reasonable" defaults so application does go pop

--- src/HiveMQClient/Model.cs
+++ src/HiveMQClient/Model.cs
@@ -8,28 +8,28 @@ namespace devMobile.IoT.NanoMqtt.Model
 {
 	internal class ApplicationSettings
 	{
-		public TimeSpan PublicationTimerDue { get; set; }
-		public TimeSpan PublicationTimerPeriod { get; set; }
+		public TimeSpan PublicationTimerDue { get; set; } = TimeSpan.FromSeconds(10);
+      public TimeSpan PublicationTimerPeriod { get; set; } = TimeSpan.FromSeconds(30);
 
-      public required string ClientId { get; set; }
-      public required string Host{ get; set; }
-      public int Port { get; set; }
-		public bool CleanStart { get; set; }
-      public bool UseTls { get; set; }
+      public required string ClientId { get; set; } = string.Empty;
+      public required string Host{ get; set; } = "";
+      public int Port { get; set; } = 8883;
+      public bool CleanStart { get; set; } = true;
+      public bool UseTls { get; set; } = true;
 
-      public required string PublishTopics { get; set; }
-      public QualityOfService PublishQualityOfService { get; set; }
-      public required string PublishContentType { get; set; }
+      public required string PublishTopics { get; set; } = string.Empty;
+      public QualityOfService PublishQualityOfService { get; set; } = QualityOfService.AtLeastOnceDelivery;
+      public required string PublishContentType { get; set; } = "";
 
-      public required string SubscribeTopics { get; set; }
-      public QualityOfService SubscribeQualityOfService { get; set; }
+      public required string SubscribeTopics { get; set; } = string.Empty;
+      public QualityOfService SubscribeQualityOfService { get; set; } = QualityOfService.AtLeastOnceDelivery;
 
-      public required string UserName { get; set; }
-      
-      public required string Password { get; set; }
+      public required string UserName { get; set; } = string.Empty;
 
-      public required string ClientCertificateFileName { get; set; }
+      public required string Password { get; set; } = string.Empty;
 
-      public required string ClientCertificatePassword { get; set; }
+      public required string ClientCertificateFileName { get; set; } = string.Empty;
+
+      public required string ClientCertificatePassword { get; set; } = string.Empty;
    }
 }
