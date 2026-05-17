b2fe1e4156b2b6966a8512a9beae355273f30047
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2026-05-18 10:16:04 +1200

    First cut of transforms

---
+++ src/MQTTLivenessMonitor/Transforms/resumed.cs
@@ -0,0 +1,12 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) May 2026, devMobile Software
+//
+using devMobile.IoT.MqttTransformer.MQTTLivenessMonitor;
+
+public class Resumed : IResumedMessageTransformer
+{
+   public string Transform(string topic, TimeSpan maximumDelay)
+   {
+      return $"Resuming message received from topic {topic} in the last {maximumDelay:hh\\:mm\\:ss}";
+   }
+}
---
+++ src/MQTTLivenessMonitor/Transforms/stopped.cs
@@ -0,0 +1,12 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) May 2026, devMobile Software
+//
+using devMobile.IoT.MqttTransformer.MQTTLivenessMonitor;
+
+public class Stopped : IStoppedMessageTransformer
+{
+   public string Transform(string topic, DateTime lastMessageReceivedUtc, TimeSpan maximumDelay)
+   {   
+      return $"No message received since {lastMessageReceivedUtc} for topic {topic} in the last {maximumDelay:hh\\:mm\\:ss}";
+   }
+}
